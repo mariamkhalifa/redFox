3db3fa9e8f3ef9e83ec85f8ceb5fc90e6d0723cf
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2019-12-01 12:19:21 -0500

    fixes

---
+++ index.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="initial-scale=1.0, width=device-width">
+	<title>Red Fox</title>
+	<link rel="icon" type="image/png" href="images/favicon.png">
+	<link href="css/reset.css" rel="stylesheet" type="text/css">
+	<link href="css/main.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:3009c9display=swap" rel="stylesheet">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/plugins/ScrollToPlugin.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.js"></script>
+</head>
+
+<body>
+	<h1 class="hidden">Red Fox</h1>
+	<div id="mainContainer">
+		<header id="mainHeader">
+			<div id="topBar"></div>
+			<div id="logoNav">
+				<nav id="mainNav">
+					<h2 class="hidden">Main Naviagation</h2>
+					<i class="fa fa-bars hamburger"></i>
+					<ul id="burgerMenu">
+						<li><a href="#" id="productsLink">Products</a></li>
+						<li><a href="#" id="aboutLink">About</a></li>
+						<li><a href="#" id="contactLink">Contact</a></li>
+					</ul>
+				</nav>
+				<div id="logo">
+					<img src="images/logo.png" alt="Logo" id="redFoxLogo">
+				</div>
+			</div>
+			</div>
+		</header>
+
+		<section id="hero">
+			<h2>Welcome to RED FOX</h2>
+			<h3>Check out  our medical products...</h3>
+			<i class="fa fa-angle-down downArrow"></i>
+		</section>
+
+		<section id="products">
+			<h1>Our Products</h1>
+			<div id="productsCon">
+				<section class="product product1">
+					<img src="images/foley-holder.jpg" alt="Holder for Foley Catheter" class="productImg">
+					<div class="productText">
+						<h3>Holder for Foley Catheter</h3>
+						<p>Our disposable holder has an adhesive and stretchable body that is water resistant and latex-free. The soft cotton material makes the holder breathable, ensuring maximum patient comfort while maintaining stretch-ability to allow movement and sustain the quality of life. Velcro tape ensures easy unfastening when needed. The holder had a writable base surface for recording the patient’s data. One size holder fits all types of Foley catheters. Our holder is manufactured at one of the best factories in China, offering great quality satisfying Canadian standards at a substantially lower cost.</p>
+					</div>
+				</section>
+				<section class="product productR product2">
+					<img src="images/endotracheal-holder.jpg" alt="Endotracheal Tube Holder" class="productImg">
+					<div class="productTextR">
+						<h3>Endotracheal Tube Holder</h3>
+						<p>Our endotracheal tube holder is fully adjustable to ensure a comfortable and secure fit around the patient’s head. The cotton material reduces skin irritation and maximizes the patient’s comfort. Velcro tape for easy adjustment and secure fastening. The screw allows for an easily adjustable fit of the tube regardless of size. Manufactured at one of the best factories in China, offering great quality satisfying Canadian standards at a substantially lower cost.</p>
+					</div>
+				</section>
+				<section class="product product3">
+					<img src="images/sticky-mat.jpg" alt="Clean Room Sticky Mat" class="productImg">
+					<div class="productText">
+						<h3>Clean Room Sticky Mat</h3>
+						<p>A multilayered adhesive mat that captures dust and undesired particles off shoes and wheeled equipment before entering the controlled environment. Offered in a standard 30 layered mat, each layer is easily peeled off revealing a new clean polyethylene film ready for traffic.  Manufactured at one of the best factories in China, offering great quality satisfying Canadian standards at a substantially lower cost.</p>
+					</div>
+				</section>
+			</div>
+		</section>
+
+		<section id="about">
+			<h1>Who We Are</h1>
+			<p>Red Fox Medical is dedicated to providing the best medical equipment for hospitals, clinics, and senior care homes. We guarantee our clients’ satisfaction by importing only the highest quality equipment and offering the lowest cost. Our passionate team strives to provide the best service for our clients through timely delivery and professional assistance. We are located in London, Ontario.</p>
+		</section>
+
+		<section id="contact">
+			<h1>Get In Touch</h1>
+			<p id="contactText">We would love to hear form you. Please use the contact form below to contact us.</p>
+			<form id="contactForm" action="contact/data_contact.php" method="POST">
+				<label for="u-name"></label>
+				<input id="u-name" type="text" name="name" placeholder="Name" required>
+				<label for="u-email"></label>
+				<input id="u-email" type="email" name="email" placeholder="Email" required>
+				<label for="u-subject"></label>
+				<input id="u-subject" type="text" name="subject" placeholder="Subject" required>
+				<label for="u-message"></label>
+				<textarea id="u-message" name="message" rows="11" cols="10" placeholder="Message" required></textarea>
+				<input type="submit" value="Submit">
+			</form>
+			<ul>
+				<li>
+					<img src="images/mail-icon-black.png" alt="Mail Icon" class="contactIcon">
+					<p>info@redfoxmedical.ca</p>
+				</li>
+				<li>
+					<img src="images/phone-icon-black.png" alt="Phone Icon" class="contactIcon">
+					<p>226-376-7375</p>
+				</li>
+			</ul>
+		</section>
+
+		<div id="scrollTop">
+			<i class="fa fa-angle-up upArrow"></i>
+		</div>
+
+		<footer id="mainFooter">
+			<p>Copyright © Red Fox Medical 2019. All Rights Reserved.</p>
+		</footer>
+		
+	</div>
+	
+	<!-- JavaScript -->
+	<script src="js/main.js"></script>
+</body>
+</html>
