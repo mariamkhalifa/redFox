3fd486f5b08542dcd5c93e45dee37e0d6a6a0b18
Mariam Khalifa <mariamk@Mariams-MacBook-Pro.local>
2020-04-23 09:00:46 -0400

    scroll down

--- js/components/HeroComponent.js
+++ js/components/HeroComponent.js
@@ -2,13 +2,30 @@ export default {
     template: `
     <section id="hero">
 			<p class="introMsg">{{ msg }}</p>
-			<i class="fa fa-angle-down fa-2x downArrow"></i>
+			<i @click="scrollDown" class="fa fa-angle-down fa-2x downArrow"></i>
 	</section>
     `,
 
     data() {
         return {
             msg: 'Your Satisfaction is Our Success'
+        }
+    },
+
+    methods: {
+        scrollDown() {
+            let mediaQuery1 = window.matchMedia('(min-width: 1024px)'),
+                mediaQuery2 = window.matchMedia('(min-width: 767px)');
+                
+            if (mediaQuery1.matches) {
+                TweenLite.to(window, 1, {scrollTo:{y:740}});
+            }
+            else if (mediaQuery2.matches) {
+                TweenLite.to(window, 1, {scrollTo:{y:860}});
+            }
+            else {
+                TweenLite.to(window, 1, {scrollTo:{y:530}});
+            }
         }
     }
 }
